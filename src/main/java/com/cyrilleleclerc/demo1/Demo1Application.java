5f82ca15ae35b5465ccb373f7b73ef3f3abb5008
Cyrille Le Clerc <cleclerc@cloudbees.com>
2017-05-23 12:43:52 +0200

    Add TODO message

--- src/main/java/com/cyrilleleclerc/demo1/Demo1Application.java
+++ src/main/java/com/cyrilleleclerc/demo1/Demo1Application.java
@@ -3,6 +3,9 @@ package com.cyrilleleclerc.demo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * TODO add a web page
+ */
 @SpringBootApplication
 public class Demo1Application {
 

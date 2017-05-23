542d1d8e91478bc27f5cadc5b1fc8b9c0282d4ec
Cyrille Le Clerc <cleclerc@cloudbees.com>
2017-05-23 12:35:53 +0200

    Add Unit attachments

--- src/test/java/com/cyrilleleclerc/demo1/Demo1ApplicationTests.java
+++ src/test/java/com/cyrilleleclerc/demo1/Demo1ApplicationTests.java
@@ -1,16 +1,36 @@
 package com.cyrilleleclerc.demo1;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileWriter;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Demo1ApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		System.out.println("Hello world");
+	}
+
+	@Test
+	public void testAttachments() throws Exception {
+		Assert.assertTrue(true);
+		File myattachment = new File("myattachment");
+
+		myattachment.createNewFile();
+		FileWriter fileWriter = new FileWriter(myattachment);
+		try {
+			fileWriter.append("This is a JUnit-attachment sample");
+			System.out.println("[[ATTACHMENT|" + myattachment.getAbsolutePath() + "]]");
+		} finally {
+			fileWriter.close();
+		}
 	}
 
 }
